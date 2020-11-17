2d13a914f51c21ba2d7774f452c6c3c86bcab707
CodeBug <se7ss@siffin.co.nz>
2020-11-17 08:25:08 +0200

    first project Commit

---
+++ src/posts/views.py
@@ -0,0 +1,54 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger 
+from django.shortcuts import render
+from .models import Post
+from marketing.models import Signup
+
+
+def index(request):
+    featured = Post.objects.filter(featured=True)
+    latest = Post.objects.order_by('-timestamp')[0:3]
+
+    if request.method == "POST":
+        email = request.POST["email"]
+        new_signup = Signup()
+        new_signup.email = email
+        new_signup.save()
+
+
+    context = {
+        'object_list': featured,
+        'latest':latest
+    }
+
+    return render(request,'index.html',context)
+
+
+def blog(request):
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 4)
+    page_request_var = 'page'
+
+    page = request.GET.get(page_request_var)
+
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
+
+    latest = Post.objects.order_by('-timestamp')[0:3]
+
+
+    context = {
+        'queryset' : paginated_queryset,
+        'page_request_var' : page_request_var,
+        'latest' : latest
+
+    }
+
+    return render(request,'blog.html', context)
+
+
+def post(request, id):
+    return render(request,'post.html',{})
